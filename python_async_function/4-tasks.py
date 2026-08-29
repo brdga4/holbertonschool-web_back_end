0a4b0f8ba7e40c7522a97654a299423add9af45b
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-29 23:51:40 +0300

    Task 4 att2

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-This module provides a function that executes multiple tasks
-concurrently and returns their results in ascending order.
+This module provides an async routine that spawns multiple tasks
+using the task_wait_random function.
 """
 import asyncio
 from typing import List
@@ -12,16 +12,11 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns task_wait_random n times with the specified max_delay
-    and returns the list of all the delays (float values).
-    The list of the delays should be in ascending order without using sort().
+    and returns the list of all delays in ascending order.
     """
-    # Create a list of n Tasks
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
 
-    # as_completed yields tasks as they finish. 
-    # Since the delay time is what the task waits for, 
-    # the ones that wait the least finish first.
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
