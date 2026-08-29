e0a0eaa19ed7be66a2804b17002bb87b2e0f8aba
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-29 23:09:09 +0300

    Task 1 att1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to concatenate two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes two strings, str1 and str2, and returns their concatenation.
+    """
+    return str1 + str2
