1c197bda266b4eee094a6ac684e03aeccc8d17ea
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-29 23:03:46 +0300

    Task 0 att1

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple function to add two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two floats, a and b, and returns their sum as a float.
+    """
+    return a + b
