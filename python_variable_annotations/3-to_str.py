7bbc9755fffcda1d50c11ebdc57fecd8ab67db10
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-29 23:16:23 +0300

    Task 3 att1

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a function that converts a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float n and returns its string representation.
+    """
+    return str(n)
