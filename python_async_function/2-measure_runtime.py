8f23488aad1054d8c008f1efa8751c8976c74d5c
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-08-29 23:46:16 +0300

    Task 2 att1

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to measure execution time of wait_n."""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the total execution time for wait_n(n, max_delay) and return total_time / n."""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start_time
+
+    return total_time / n
