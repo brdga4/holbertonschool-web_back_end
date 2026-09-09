4b7a6665955ceb02e98a228874bc34733e124194
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-09-09 15:33:46 +0300

    Task 2 att1

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Simple helper function to return a tuple of start and end indexes
+"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for
+    those particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Gets the page content from the starting index to the ending index
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            List[List]: _description_
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        data = self.get_page(page, page_size)
+        pageSize = len(data)
+        prev_page = page - 1 if page > 1 else None
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page < total_pages else None
+
+        return {
+            "page_size": pageSize,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
