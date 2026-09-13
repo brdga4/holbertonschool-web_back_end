eac9cf1371b0bd5f18702753fef9d4800be2b213
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-09-13 17:23:01 +0300

    Task 3 att6

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -5,12 +5,12 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import Dict, List
+from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,7 +18,8 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -28,19 +29,24 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0"""
+        """Dataset indexed by sorting position, starting at 0
+        """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Returns a dictionary with deletion-resilient pagination data.
+        Return dict of deletion-resilient pagination data.
         """
-        assert type(index) == int and type(page_size) == int
-        assert 0 <= index < len(self.dataset())
+        if index is None:
+            index = 0
+
+        assert isinstance(index, int) and 0 <= index < len(self.dataset())
 
         indexed_data = self.indexed_dataset()
         data = []
@@ -49,12 +55,11 @@ class Server:
         while len(data) < page_size and next_index < len(self.dataset()):
             if next_index in indexed_data:
                 data.append(indexed_data[next_index])
-
             next_index += 1
 
         return {
             "index": index,
-            "data": data,
-            "page_size": len(data),
             "next_index": next_index,
+            "page_size": len(data),
+            "data": data
         }
