ee8d5f95ea09a53904834e6e805f8ba6fc225cb2
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-09-14 21:01:46 +0300

    Add gitignore

---
+++ NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Shebang for Task 8
+"""
+
+
+def list_all(mongo_collection):
+    """_summary_
+
+    Args:
+        mongo_collection (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return list(mongo_collection.find())
