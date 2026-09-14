b75f5c428295f9f15cc7dac48d86a42830cd7991
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-09-14 21:33:35 +0300

    Task 10 att1

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Shebang for Task 10
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """_summary_
+
+    Args:
+        mongo_collection (_type_): _description_
+        name (_type_): _description_
+        topics (_type_): _description_
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
