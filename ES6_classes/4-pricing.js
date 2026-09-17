3c1107e3ca8880f7ab71bdaaa2a41c96cbfe4c2a
Basem Tashkandi <basem.tashkandi@gmail.com>
2026-09-17 20:41:08 +0300

    Task 4 att1

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
